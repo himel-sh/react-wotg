4e2531eafda47c0dc61761ef6e1c2a47ca17bbbd
Himel <shahriarhasan221@gmail.com>
2025-09-19 11:41:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h1>React World On The Go(WOTG)</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
